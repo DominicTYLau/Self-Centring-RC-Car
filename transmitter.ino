5f1628975a7276a52c3447f9abc345b649e5b2b6
Dominic Lau <128517103+DominicTYLau@users.noreply.github.com>
2024-02-19 21:32:57 -0500

    Update transmitter.ino

--- transmitter.ino
+++ transmitter.ino
@@ -22,10 +22,9 @@ void setup()
   digitalWrite(switchPin, HIGH);
 
   while (!Serial)
-    ; // wait for serial port to connect. Needed for Leonardo only
+    ; 
   if (!nrf24.init())
     Serial.println("init failed");
-  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
   if (!nrf24.setChannel(1))
     Serial.println("setChannel failed");
   if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
@@ -37,15 +36,14 @@ void loop()
   if(digitalRead(switchPin) == false){
     status = !status;
   }
-
   xValue = analogRead(VRX_PIN);
+  // Input from joystick
   yValue = analogRead(VRY_PIN);
   if(yValue > 800){
     yValue = 800;
   }
 
   String message = String(xValue) + " " + String(yValue) +" "+ String(status);
-  Serial.println(message);
 
   char data[message.length() + 1];
   message.toCharArray(data, sizeof(data));
